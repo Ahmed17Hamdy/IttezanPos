43ce7ed43b74601d2131471ce1b81aafc1a61667
AhmedHamdy <the17hawk@live.com>
2019-08-19 11:37:37 +0300

    Add project files.

---
+++ IttezanPos/IttezanPos/Views/Intro/SplashPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IttezanPos.Views.Intro
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SplashPage : ContentPage
+    {
+        public SplashPage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+
+            await splashImage.FadeTo(1,3000,Easing.Linear);
+
+       //     SetMainPage();
+            //  await splashImage.ScaleTo(150, 1200, Easing.Linear);
+            //After loading  MainPage it gets Navigated to our new Page
+        }
+        private double width = 0;
+        private double height = 0;
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height); //must be called
+            if (this.width != width || this.height != height)
+            {
+                this.width = width;
+                this.height = height;
+                //reconfigure layout
+            }
+        }
+    }
+}
---
+++ IttezanPos/IttezanPos/Views/Master/MasterPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IttezanPos.Views.Master
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterPage : MasterDetailPage
+    {
+        public MasterPage()
+        {
+            InitializeComponent();
+            masterPage.listView.ItemSelected += OnItemSelected;
+            masterPage.newlist.ItemSelected += OnItemSelected;
+            masterPage.listhelp.ItemSelected += OnItemSelected;
+        }
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as MasterPageItem;
+            if (item != null)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+                masterPage.listView.SelectedItem = null;
+                masterPage.newlist.SelectedItem = null;
+                masterPage.listhelp.SelectedItem = null;
+                IsPresented = false;
+            }
+        }
+    }
+}
